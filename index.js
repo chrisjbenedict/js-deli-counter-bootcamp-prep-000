164f1adc71b854c75c82be8dd90b2247489421ec
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-18 20:30:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function takeANumber(line, name) {
   return `Welcome, ${name}. You are number ${line.length} in line.`
 };
 
-function nowServing(line) {
-  if (line.length === 0)
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0)
     return "There is nobody waiting to be served!"
   else {
-    return `Currently serving ${line.shift()}.`
+    return `Currently serving ${katzDeliLine.shift()}.`
     }
 };
 
